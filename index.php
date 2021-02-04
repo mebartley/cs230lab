2b8c6a9723a9fb5f66def3f53511f50d07f49eda
mebartley <mebartley@mix.wvu.edu>
2021-02-04 00:36:07 -0500

    Lab 2 - About

---
+++ index.php
@@ -0,0 +1,62 @@
+<main>
+    <link rel = "stylesheet" href = "css/about.css">
+    <div class = "about">
+        <h1>About Us</h1>
+        <p>One Direction: Music Makers to Music Reviewers</p>
+    </div>
+    <div class = "row">
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/hs.jpg">
+                <div class = "container">
+                    <h2>Music Critic</h2>
+                    <p>Harry Styles</p>
+                    <p>Email: hs@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/lp.jpeg">
+                <div class = "container">
+                    <h2>Music Critic</h2>
+                    <p>Liam Payne</p>
+                    <p>Email: lp@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/lt1.jpg">
+                <div class = "container">
+                    <h2>Music Critic</h2>
+                    <p>Louis Tomlinson</p>
+                    <p>Email: lt@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/nh.png">
+                <div class = "container">
+                    <h2>Creative Designer</h2>
+                    <p>Niall Horan</p>
+                    <p>Email: nh@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src = "images/zm.jpg">
+                <div class = "container">
+                    <h2>Review Editor</h2>
+                    <p>Zayn Malik</p>
+                    <p>Email: zm@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "row1">
+        <p>Although no longer a band, One Direction members now review music.  They have toured the world and made counless albums together as well as on their own.  With much experience in the music industry, their reviews can be trusted.</p> 
+    </div>
+</main>
