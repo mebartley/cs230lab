7eeb2903a494c40925b6bf50ad563d821ef038e2
Morgan <77845211+mebartley@users.noreply.github.com>
2021-03-25 19:01:51 -0400

    Update header.php
    Spelling error fix in header

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TMPR</title>
+    <title>Music</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
